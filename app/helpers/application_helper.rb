289489df106d707cc2ec0bde58aac04ff87d7909
Derek DiCillo <dddicillo@gmail.com>
2014-04-03 17:42:37 -0400

    Add title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  full_title(page_title) {
+    base_title = "Flipturn"
+    if base_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  }
 end

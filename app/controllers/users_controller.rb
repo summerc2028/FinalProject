36baa1cfa5829889819f49c55f3765b2419be5ae
Derek DiCillo <dddicillo@gmail.com>
2014-04-07 08:29:40 -0400

    Format validation errors and flashes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Account Successfully Created!"
       redirect_to @user
     else
       render 'new'
@@ -18,6 +19,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit( :name, :email, :password, :password_confirmation)
+      params.require(:user).permit( :name, :username, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 	validates :email, 	presence: true,
 						format: { with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { minimum: 6 }
 	has_secure_password
 	validates :password, length: { minimum: 8 }
 end

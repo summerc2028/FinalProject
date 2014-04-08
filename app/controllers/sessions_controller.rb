5d27329937fc8a3e5061a4021e747127d4f203cc
Derek DiCillo <dddicillo@gmail.com>
2014-04-08 19:19:06 -0400

    Work on session persistence

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username].downcase)
     if user && user.authenticate(params[:password])
-      sign_in user
+      sign_in user, params[:remember_me]
       flash[:success] = "Successfully Signed In!"
       redirect_to user_path(session[:user_id])
     else

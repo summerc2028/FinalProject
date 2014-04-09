dc807643ff23a8ffbe8394d18fc6a1634f0b3b63
Derek DiCillo <dddicillo@gmail.com>
2014-04-08 20:48:01 -0400

    Finish sessioninggit add .git add .

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,18 @@
 module SessionsHelper
 
   def sign_in(user, remember)
-    auth_token = User.new_auth_token
+    auth_token = user.auth_token
     if remember
       cookies.permanent[:auth_token] = auth_token
     else
       cookies[:auth_token] = auth_token
     end
     self.current_user = user
+  end
+
+  def sign_out
+    cookies.delete(:auth_token)
+    self.current_user = nil
   end
 
   def signed_in?
@@ -19,6 +24,6 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by_auth_token cookies[:auth_token] if cookies[:auth_token]
+    @current_user = User.find_by_auth_token cookies[:auth_token] if cookies[:auth_token]
   end
 end

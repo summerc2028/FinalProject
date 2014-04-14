4f5adad1b59d73968bc8686fd440b8e11b32d2fa
Derek DiCillo <dddicillo@gmail.com>
2014-04-13 23:31:43 -0400

    Add activity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,10 @@
 class Activity < ActiveRecord::Base
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :day, presence: true
+  validates :time, presence: true
+  VALID_LENGTH_REGEX = /\A(0|1)?\d:[0-5]\d\z/
+  validates :length, presence: true, format: { with: VALID_LENGTH_REGEX }
+  validates :location, presence: true, length: { maximum: 16 }
   belongs_to :user
 end

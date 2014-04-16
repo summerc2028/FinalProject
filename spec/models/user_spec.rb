cad016b17ffab55bd403c7b0c4683f0d3104beb7
Derek DiCillo <dddicillo@gmail.com>
2014-04-15 22:06:37 -0400

    Write more model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -248,48 +248,53 @@ describe User do
     it { should_not be_valid }
   end
 
-  # describe "micropost associations" do
-  #
-  #   before { @user.save }
-  #   let!(:older_micropost) do
-  #     FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-  #   end
-  #   let!(:newer_micropost) do
-  #     FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-  #   end
-  #
-  #   it "should have the right microposts in the right order" do
-  #     expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
-  #   end
-  #
-  #   it "should destroy associated microposts" do
-  #     microposts = @user.microposts.to_a
-  #     @user.destroy
-  #     expect(microposts).not_to be_empty
-  #     microposts.each do |micropost|
-  #       expect(Micropost.where(id: micropost.id)).to be_empty
-  #     end
-  #   end
-  #
-  #   describe "status" do
-  #     let(:unfollowed_post) do
-  #       FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-  #     end
-  #     let(:followed_user) { FactoryGirl.create(:user) }
-  #
-  #     before do
-  #       @user.follow!(followed_user)
-  #       3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
-  #     end
-  #
-  #     its(:feed) { should include(newer_micropost) }
-  #     its(:feed) { should include(older_micropost) }
-  #     its(:feed) { should_not include(unfollowed_post) }
-  #     its(:feed) do
-  #       followed_user.microposts.each do |micropost|
-  #         should include(micropost)
-  #       end
-  #     end
-  #   end
-  # end
+  describe "associated activities" do
+
+    before { @user.save }
+    let!(:user_activity) do
+      FactoryGirl.create(:activity, user: @user)
+    end
+
+    it "should destroy associated activities" do
+      activities = @user.activities.to_a
+      @user.destroy
+      expect(activities).not_to be_empty
+      activities.each do |activity|
+        expect(Activity.where(id: activity.id)).to be_empty
+      end
+    end
+  end
+
+  describe "associated exercises" do
+
+    before { @user.save }
+    let!(:user_exercise) do
+      FactoryGirl.create(:exercise, user: @user)
+    end
+    it "should destroy associated exercises" do
+      exercises = @user.exercises.to_a
+      @user.destroy
+      expect(exercises).not_to be_empty
+      exercises.each do |exercise|
+        expect(Exercise.where(id: exercise.id)).to be_empty
+      end
+    end
+  end
+
+  describe "associated foods" do
+
+    before { @user.save }
+    let!(:user_food) do
+      FactoryGirl.create(:food, user: @user)
+    end
+
+    it "should destroy associated foods" do
+      foods = @user.foods.to_a
+      @user.destroy
+      expect(foods).not_to be_empty
+      foods.each do |food|
+        expect(Food.where(id: food.id)).to be_empty
+      end
+    end
+  end
 end

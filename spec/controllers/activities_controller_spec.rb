45eb5d19afb567249503d22d267a4c45331af950
Derek DiCillo <dddicillo@gmail.com>
2014-04-16 11:57:05 -0400

    Begin fixing controller test

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,63 +1,67 @@
 require 'spec_helper'
 
-class ApplicationController < ActionController::Base
-    class AccessDenied < StandardError; end
+# class ApplicationController < ActionController::Base
+#     class AccessDenied < StandardError; end
+#
+#     rescue_from AccessDenied, :with => :access_denied
+#
+#     private
+#     def access_denied
+#         redirect_to "/401.html"
+#     end
+# end
 
-    rescue_from AccessDenied, :with => :access_denied
+describe ActivitiesController do
 
-    private
-    def access_denied
-        redirect_to "/401.html"
-    end
-end
+  let(:user) { FactoryGirl.create(:user) }
+  let(:activity) { user.activities.create( name: "Sample Activity", day: Date.current,
+                    time: Time.current, length: "4:00",
+                    location: "Sample Location") }
 
-describe ApplicationController do
-  controller do
-    def index
-      raise ApplicationController::AccessDenied
+  # controller do
+  #   def index
+  #     raise ApplicationController::AccessDenied
+  #   end
+  # end
+
+  # describe "handling AccessDenied exceptions" do
+  #     it "redirects to the /401.html page" do
+  #       get :index
+  #       expect(response).to redirect_to("/401.html")
+  #   end
+  # end
+
+
+  describe "#new" do
+      it "renders the new view" do
+        get :new
+        response.should render_template :new
+      end
   end
-end
 
-describe "handling AccessDenied exceptions" do
-    it "redirects to the /401.html page" do
-      get :index
-      expect(response).to redirect_to("/401.html")
+  describe "#create" do
+      it "creates a new activity" do
+
+
+
+      end
   end
-end
 
-describe "#create" do 
-    it "creates a new activity" do
-    
+  describe "#show" do
+      it do
 
+      end
+  end
 
-    end
-end
+  describe "#destroy" do
+      it do
 
-describe "#show" do
-    it 
+      end
+  end
 
-    end
-end
+  describe "#update" do
+      it do
 
-describe "#destroy" do
-    it 
-
-    end
-end
-
-
-describe "#new" do
-    it 
-
-    end
-end
-
-describe "#update" do
-    it 
-
-    end
-end
-
-
-
+      end
+  end
 end

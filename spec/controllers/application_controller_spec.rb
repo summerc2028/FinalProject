186d52192713fa650cba42065f0753b8e1fcb814
Amna Ali Khan <amna@amna-VirtualBox.(none)>
2014-04-16 11:50:03 -0400

    working on test cases

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,5 +2,24 @@ require 'spec_helper'
 
 describe ApplicationController do
 
+	controller(ApplicationController) do
+		def index
+		end
+	end
+
+	describe "" do
+		before(:each) do
+			@admin = create(:admin)
+			@request.env["devise.mapping"] = Devise.mappings[:admin]
+		end
+
+		context "when admin is logged in" do
+			it "redirects admin to profile page" do
+				get :index
+				response.should redirect_to profile
+			end
+		end
+	end
 
 end
+

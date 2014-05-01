3808ff213a40706bd15db743f09db2293122ff5a
Derek DiCillo <dddicillo@gmail.com>
2014-05-01 15:20:28 -0400

    Fix another migration error

--- db/migrate/20140415222720_add_calories_to_exercises.rb
+++ db/migrate/20140415222720_add_calories_to_exercises.rb
@@ -1,6 +1,6 @@
 class AddCaloriesToExercises < ActiveRecord::Migration
   def change
   	add_column :exercises, :Calories, :Integer
-  	remove_column :exercises, :Length
+  	remove_column :exercises, :length
   end
 end

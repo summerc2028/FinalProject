94f502da10407dd80203282cea6ef7f9c9ab0402
Derek DiCillo <dddicillo@gmail.com>
2014-05-01 15:18:23 -0400

    Fix migration error

--- db/migrate/20140414034839_remove_length_from_activities.rb
+++ db/migrate/20140414034839_remove_length_from_activities.rb
@@ -2,6 +2,5 @@ class RemoveLengthFromActivities < ActiveRecord::Migration
   def change
     remove_column :activities, :length
     add_column :activities, :length, :string
-    remove_column :users, :length
   end
 end
